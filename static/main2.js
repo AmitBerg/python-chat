e647e72f01998a75b39719d2b0450b1f2346aeeb
Amit Grossberg <Grossberg.amit@gmail.com>
2018-01-23 12:55:28 +0200

    Revert "first commit"
    This reverts commit f86f26695f9fab04e8196253524907b804a6db3d.

--- static/main2.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-    // Correctly decide between ws:// and wss://
-    let ws_scheme = window.location.protocol === "https:" ? "wss" : "ws";
-    let ws_path = ws_scheme + '://' + window.location.host + "/chat/stream/";
-    let socket = new ReconnectingWebSocket(ws_path);
-});
